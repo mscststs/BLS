83f6992f6d4e2e5ce3aa943d9045836fb2a9a123
mscststs <mscststs@qq.com>
2018-06-11 00:37:43 +0800

    修改小黑屋回避为按时段

--- src/renderer/components/stocks/BlackroomBlock.vue
+++ src/renderer/components/stocks/BlackroomBlock.vue
@@ -4,8 +4,26 @@
           <el-col :span="24">
               <el-form :model="form" inline>
                 <el-form-item label="禁止时间段">
-                    <el-input v-model="form.input" disabled></el-input>
+                      <el-time-picker
+                        is-range
+                        arrow-control
+                        v-model="form.time"
+                        range-separator="至"
+                        start-placeholder="开始时间"
+                        end-placeholder="结束时间"
+                        placeholder="选择时间范围">
+                    </el-time-picker>
+                    <el-button type="success" @click="BlockTime">回避该时段</el-button>
                 </el-form-item>
+              </el-form>
+          </el-col>
+          <el-col :span="24" style="margin-bottom:14px;">
+              <div class="block-time" v-for="time in BlockTimeList" :key="time.desc">
+                  {{time.desc}} <el-button type="primary" plain size="small" @click="RemoveTimeBlock(time)"> X </el-button>
+              </div>
+          </el-col>
+          <el-col :span="24">
+              <el-form :model="form" inline>
                 <el-form-item label="小电视回避">
                     <el-switch v-model="form.smalltv"></el-switch>
                 </el-form-item>
@@ -16,18 +34,6 @@
                     <el-switch v-model="form.online"></el-switch>
                 </el-form-item>
               </el-form>
-          </el-col>
-          <el-col :span="24">
-              <el-slider
-                    v-model="form.time"
-                    range
-                    :show-tooltip="true"
-                    show-stops
-                    @change="UpdateValue"
-                    :min="0"
-                    :step="1"
-                    :max="24">
-                </el-slider>
           </el-col>
           <el-col :span="24" style="margin-top:24px">
               <div id="Heatmap">
@@ -44,9 +50,10 @@ import echarts from "echarts"
 export default {
     data(){
         return{
+            BlockTimeList:[],
             myChart:{},
             form:{
-                time:[1,9],
+                time:[new Date(),new Date()],
                 input:"01:00 ~ 09:00",
                 smalltv:true,
                 raffle:true,
@@ -80,8 +87,100 @@ export default {
     mounted(){
         this.Addlistener();
         this.DrawHeatMap(true);
+        this.BlockTimeList = this.$store.data.BlockTimeList || [];
     },
     methods:{
+        RemoveTimeBlock(time){
+            let n = [];
+            for(let t of this.BlockTimeList){
+                if(t.desc == time.desc){
+
+                }else{
+                    n.push(t);
+                }
+            }
+            this.BlockTimeList = n;
+            this.$store.update(data=>{
+                data.BlockTimeList=  this.BlockTimeList;
+            })
+        },
+        getSeconds(time){
+            let hour = time.getHours();
+            let minute = time.getMinutes();
+            let second = time.getSeconds();
+            return ((hour*60)+minute)*60+second; //得到相对于0点0分0秒的偏移值
+        },
+        formatTime(date = new Date(), fmt = "YYYY-MM-DD HH:mm:ss") {
+            date = typeof date === "number" ? new Date(date) : date;
+            var o = {
+                "M+": date.getMonth() + 1,
+                "D+": date.getDate(),
+                "h+": date.getHours() % 12 === 0 ? 12 : date.getHours() % 12,
+                "H+": date.getHours(),
+                "m+": date.getMinutes(),
+                "s+": date.getSeconds(),
+                "q+": Math.floor((date.getMonth() + 3) / 3),
+                S: date.getMilliseconds()
+            };
+            var week = {
+                "0": "\u65e5",
+                "1": "\u4e00",
+                "2": "\u4e8c",
+                "3": "\u4e09",
+                "4": "\u56db",
+                "5": "\u4e94",
+                "6": "\u516d"
+            };
+            if (/(Y+)/.test(fmt)) {
+                fmt = fmt.replace(
+                RegExp.$1,
+                (date.getFullYear() + "").substr(4 - RegExp.$1.length)
+                );
+            }
+            if (/(E+)/.test(fmt)) {
+                fmt = fmt.replace(
+                RegExp.$1,
+                (RegExp.$1.length > 1
+                    ? RegExp.$1.length > 2 ? "\u661f\u671f" : "\u5468"
+                    : "") + week[date.getDay() + ""]
+                );
+            }
+            for (var k in o) {
+                if (new RegExp("(" + k + ")").test(fmt)) {
+                fmt = fmt.replace(
+                    RegExp.$1,
+                    RegExp.$1.length === 1
+                    ? o[k]
+                    : ("00" + o[k]).substr(("" + o[k]).length)
+                );
+                }
+            }
+            return fmt;
+        },
+        BlockTime(){
+            let Time1 = this.form.time[0];
+            let Time2 = this.form.time[1];
+            if(Time1 && Time1.getHours && typeof Time1.getHours == "function"){
+                if(this.getSeconds(Time1)>=this.getSeconds(Time2)){
+                    this.$eve.emit("error","时间不合法");
+                }else{
+                    let s = {
+                        min:this.getSeconds(Time1),
+                        max:this.getSeconds(Time2),
+                        desc:`${this.formatTime(Time1,"HH:mm:ss")} - ${this.formatTime(Time2,"HH:mm:ss")}`,
+                    };
+                    this.RemoveTimeBlock(s);
+                    //防止创建同样的规则
+
+                    this.BlockTimeList.push(s);
+                    this.BlockTimeList.sort((a,b)=>a.min-b.min);
+
+                    this.$store.update(data=>{
+                        data.BlockTimeList = this.BlockTimeList;
+                    });
+                }
+            }
+        },
         Addlistener(){
             this.$eve.on("dm_SmallTv",(data)=>{
                 this.HeatResolve(data);
@@ -215,13 +314,14 @@ export default {
         UpdateValue(){
             this.form.input = `${(this.form.time[0]<10?"0":"")+this.form.time[0]}:00 ~ ${(this.form.time[1]<10?"0":"")+this.form.time[1]}:00`
         },
-        isBlockTime(){
-            let da = new Date();
-            let NowHour = da.getHours();
-            if(NowHour>=this.form.time[0] && NowHour<this.form.time[1]){
-                return true;
+        isBlockTime(time = new Date()){
+            let sec = this.getSeconds(time);
+            for(let rg of this.BlockTimeList){
+                if(sec>=rg.min && sec <rg.max){
+                    return false;
+                }
             }
-            return false;
+            return true;
         }
     },
 }
@@ -233,6 +333,16 @@ export default {
         width: 100%;
         height:400px;
 
+    }
+    div.block-time{
+        display: inline-block;
+        padding: 3px 7px;
+        font-size: 14px;
+        background-color: #e5f1fa;
+        margin-right: 14px;
+        margin-bottom:3px;
+        color:#000;
+        border-radius: 4px;
     }
 
 </style>
